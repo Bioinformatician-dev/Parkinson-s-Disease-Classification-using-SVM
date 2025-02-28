d2fa8c3e29012cc80718492ce9f034c51fa546ee
Salma <salmahafeez575@gmail.com>
2025-02-28 18:15:00 +0500

    Update Dataset.py

--- Dataset.py
+++ Dataset.py
@@ -1,8 +1,10 @@
-import os
+
+!pip install kaggle
 !mkdir -p ~/.kaggle
 !mv kaggle.json ~/.kaggle/
 !chmod 600 ~/.kaggle/kaggle.json
 !kaggle datasets download -d vikasukani/parkinsons-disease-data-set
+import os
 import zipfile
 zip_file = "parkinsons-disease-data-set.zip"
 extract_folder = "parkinsons_data"
